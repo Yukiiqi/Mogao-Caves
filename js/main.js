a4dcf364a95eeafbc7453a2f17ed76cfa6c77682
Yukiiqi <Yukiiqi@users.noreply.github.com>
2021-11-24 00:24:28 +0000

    deploy: 7790e0e9954044df77a1db4eacbd71b55247a70e

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+function setClass(className) {
+  document.querySelector("div").className = className;
+}
 
+var articles = document.querySelectorAll("article");
+var buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    var buttonid = this.id;
+    var modifiedid = buttonid.replace("button-", "");
+    var textid = document.getElementById(modifiedid);
+    textid.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+var sections = document.querySelectorAll("section");
+let options = {
+  rootMargin: "0px 0px -200px 0px",
+  threshold: 0,
+};
+/* Just making the observer */
+var observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(`${entry.target.id} is intersecting.`);
+      setClass(entry.target.dataset.class);
+    }
+  });
+}, options);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
